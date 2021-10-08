ffbcc07c92a2822c8ff603ccd9fce6f7da98bc23
unknown <unaxlasa@gmail.com>
2021-10-08 16:56:32 +0200

    08/10/21

--- Lab/03-GPIO/gpio/gpio/main.cpp
+++ Lab/03-GPIO/gpio/gpio/main.cpp
@@ -12,6 +12,8 @@
 
 /* Defines -----------------------------------------------------------*/
 #define LED_GREEN   PB5     // AVR pin where green LED is connected
+#define LED_BLUE PB6
+#define BUTTON PB7
 #define BLINK_DELAY 500
 #ifndef F_CPU
 # define F_CPU 16000000     // CPU frequency in Hz required for delay, 16MHz
@@ -29,6 +31,14 @@
  *           from user-defined GPIO library is used.
  * Returns:  none
  **********************************************************************/
+//Function declaration
+void GPIO_config_output(volatile uint8_t *reg_name, uint8_t pin_num);
+void GPIO_config_input_pullup(volatile uint8_t *reg_name, uint8_t pin_num);
+void GPIO_write_low(volatile uint8_t *reg_name, uint8_t pin_num);
+void GPIO_write_high(volatile uint8_t *reg_name, uint8_t pin_num);
+void GPIO_toggle(volatile uint8_t *reg_name, uint8_t pin_num);
+uint8_t GPIO_read(volatile uint8_t *reg_name, uint8_t pin_num);
+
 int main(void)
 {
     // Green LED at port B
@@ -36,19 +46,22 @@ int main(void)
     GPIO_write_low(&PORTB, LED_GREEN);
 
     // Configure the second LED at port C
-
-
+	GPIO_config_output(&DDRC, LED_BLUE);
+	GPIO_write_high(&PORTC, LED_BLUE);
     // Configure Push button at port D and enable internal pull-up resistor
-
+	GPIO_config_input_pullup(&DDRD, BUTTON);
 
     // Infinite loop
     while (1)
     {
         // Pause several milliseconds
         _delay_ms(BLINK_DELAY);
-
-        // WRITE YOUR CODE HERE
-		GPIO_toggle(&PORTB, LED_GREEN);
+		while(GPIO_read(&PORTD,BUTTON))
+		{
+			GPIO_toggle(&PORTB, LED_GREEN);
+			GPIO_toggle(&PORTB, LED_BLUE);
+			_delay_ms(BLINK_DELAY);
+		}
     }
 
     // Will never reach this
