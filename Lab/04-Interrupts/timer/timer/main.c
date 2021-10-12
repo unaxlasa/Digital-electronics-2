1237d72d52f60b2166f66a5e5ea2e8bec04c0113
unknown <unaxlasa@gmail.com>
2021-10-12 14:50:52 +0200

    12/10/21

---
+++ Lab/04-Interrupts/timer/timer/main.c
@@ -0,0 +1,72 @@
+//***********************************************************************
+
+/* Defines -----------------------------------------------------------*/
+#define LED_D1  PB5
+#define LED_D2  PB4
+#define LED_D3  PB3
+#define LED_D4  PB2
+#define BUTTON_S1 PC1
+
+/* Includes ----------------------------------------------------------*/
+#include <avr/io.h>         // AVR device-specific IO definitions
+#include <avr/interrupt.h>  // Interrupts standard C library for AVR-GCC
+#include "gpio.h"           // GPIO library for AVR-GCC
+#include "timer.h"          // Timer library for AVR-GCC
+
+/* Function definitions ----------------------------------------------*/
+/**********************************************************************
+ * Function: Main function where the program execution begins
+ * Purpose:  Toggle one LED on the Multi-function shield using 
+             the internal 8- or 16-bit Timer/Counter.
+ * Returns:  none
+ **********************************************************************/
+int main(void)
+{
+    // Configuration of LED(s) at port B
+    GPIO_config_output(&DDRB, LED_D1);
+    GPIO_write_low(&PORTB, LED_D1);
+
+    // Configuration of 16-bit Timer/Counter1 for LED blinking
+    // Set the overflow prescaler to 262 ms and enable interrupt
+    TIM1_overflow_262ms();
+    TIM1_overflow_interrupt_enable();
+	
+	//COnfigure a push button at port C
+	GPIO_config_input_nopull(&DDRC, BUTTON_S1);
+
+    // Enables interrupts by setting the global interrupt mask
+    sei();
+
+    // Infinite loop
+    while (1)
+    {
+        /* Empty loop. All subsequent operations are performed exclusively 
+         * inside interrupt service routines ISRs */
+		
+		//Check push button state
+		if (GPIO_read(&PINC, BUTTON_S1) == 0) //Push button is pressed
+		{
+			TIM1_overflow_33ms();
+		}
+		else // Push button is released
+		{ 
+			TIM1_overflow_262ms();
+		}
+    }
+
+    // Will never reach this
+    return 0;
+}
+
+/* Interrupt service routines ----------------------------------------*/
+/**********************************************************************
+ * Function: Timer/Counter1 overflow interrupt
+ * Purpose:  Toggle D1 LED on Multi-function shield.
+ **********************************************************************/
+ISR(TIMER1_OVF_vect)
+{
+
+    // WRITE YOUR CODE HERE
+	GPIO_toggle(&PORTB, LED_D1);
+
+}
